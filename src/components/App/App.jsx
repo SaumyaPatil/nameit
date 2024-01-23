c6c8a79ecbfe73e70ad6cd6e9d8f30032badb0e7
SaumyaPatil <saupatil10dec@gmail.com>
2024-01-23 20:25:20 +0530

    new file:   .prettierrc 	modified:   README.md 	modified:   package-lock.json 	modified:   package.json 	modified:   public/index.html 	deleted:    src/App.css 	deleted:    src/App.js 	deleted:    src/App.test.js 	new file:   src/components/App/App.css 	new file:   src/components/App/App.jsx 	new file:   src/components/Header/Header.css 	new file:   src/components/Header/Header.jsx 	new file:   src/components/NameCard/NameCard.css 	new file:   src/components/NameCard/NameCard.jsx 	new file:   src/components/ResultsContainer/ResultsContainer.css 	new file:   src/components/ResultsContainer/ResultsContainer.jsx 	new file:   src/components/SearchBox/SearchBox.css 	new file:   src/components/SearchBox/SearchBox.jsx 	modified:   src/index.js 	deleted:    src/logo.svg

---
+++ src/components/App/App.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import Header from '../Header/Header';
+import './App.css';
+import SearchBox from '../SearchBox/SearchBox';
+import ResultsContainer from '../ResultsContainer/ResultsContainer';
+
+const name = require('@rstacruz/startup-name-generator');
+
+const App = () =>{
+    const headerText = useState('Name It!');
+    const [headerExpanded, setheaderExpanded] = useState(true);
+    const [suggestedNames, setsuggestedNames] = useState([]);
+ 
+    const handleInputChange = (inputText) =>{
+        setheaderExpanded(!(inputText.length > 0));
+        setsuggestedNames(inputText.length > 0 ? name(inputText) : []);
+    };
+    return(
+        <>
+            <Header headerExpanded={headerExpanded} headTitle={headerText}/>
+            <SearchBox onInputChange={handleInputChange}/>
+            <ResultsContainer suggestedNames={suggestedNames}/>
+        </>
+    )
+}
+
+export default App;
---
+++ src/components/NameCard/NameCard.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './NameCard.css';
+
+const nameCheapUrl = 'https://www.youtube.com/redirect?event=video_description&redir_token=QUFFLUhqbHdlcWJobHk1LVpjMkNVeVRxdmZYZDRRSVhJd3xBQ3Jtc0tsMFBnZE9sV1Q0bGFtYVFCUmIwWTBtbU1UaWxlbUd0b3ZzSGw4dVZweXItcDNiNEl6U3RJbTE2SHlDT3JiMVJ5SmNSVmI1ZnViaFA0UmFlTDZWUTFaRVY1MzJkdGxRTzcxbG05NllldEhMUDhIUkdiQQ&q=https%3A%2F%2Fwww.namecheap.com%2Fdomains%2Fregistration%2Fresults%2F%3Fdomain%3D%27%3B&v=PIrcgjaPPaU';
+
+const NameCard = ({suggestedName}) =>{
+    return(
+        <a className = "card-link" href={`${nameCheapUrl}${suggestedName}`}>
+            <div className="result-name-card">
+                <p className="result-name">{suggestedName}</p>
+            </div>
+        </a>
+    )
+}
+
+export default NameCard;
